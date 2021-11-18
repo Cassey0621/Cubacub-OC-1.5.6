03249828fecb7ab72767b173fa08153a7f1ba18c
Cassey0621 <mc.matubis0627@student.tsu.edu.ph>
2021-11-18 21:49:33 +0800

    Update Config

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://cubacubhardware.tech/');
+define('HTTP_SERVER', 'https://yourwebsite.tld/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://cubacubhardware.tech/');
+define('HTTPS_SERVER', 'https://yourwebsite.tld/');
 
 // DIR
-define('DIR_APPLICATION', '/home/fhw9plzvzuza/public_html/catalog/');
-define('DIR_SYSTEM', '/home/fhw9plzvzuza/public_html/system/');
-define('DIR_DATABASE', '/home/fhw9plzvzuza/public_html/system/database/');
-define('DIR_LANGUAGE', '/home/fhw9plzvzuza/public_html/catalog/language/');
-define('DIR_TEMPLATE', '/home/fhw9plzvzuza/public_html/catalog/view/theme/');
-define('DIR_CONFIG', '/home/fhw9plzvzuza/public_html/system/config/');
-define('DIR_IMAGE', '/home/fhw9plzvzuza/public_html/image/');
-define('DIR_CACHE', '/home/fhw9plzvzuza/public_html/system/cache/');
-define('DIR_DOWNLOAD', '/home/fhw9plzvzuza/public_html/download/');
-define('DIR_LOGS', '/home/fhw9plzvzuza/public_html/system/logs/');
+define('DIR_APPLICATION', '/home/username/public_html/catalog/');
+define('DIR_SYSTEM', '/home/username/public_html/system/');
+define('DIR_DATABASE', '/home/username/public_html/system/database/');
+define('DIR_LANGUAGE', '/home/username/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/username/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/username/public_html/system/config/');
+define('DIR_IMAGE', '/home/username/public_html/image/');
+define('DIR_CACHE', '/home/username/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/username/public_html/download/');
+define('DIR_LOGS', '/home/username/public_html/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'cubacubdbuser');
-define('DB_PASSWORD', '@Uno23quatro5');
-define('DB_DATABASE', 'dababase_cub');
+define('DB_HOSTNAME', 'databasehost');
+define('DB_USERNAME', 'databaseuser');
+define('DB_PASSWORD', 'databasepass');
+define('DB_DATABASE', 'dababasename');
 define('DB_PREFIX', 'cah_');
 ?>
